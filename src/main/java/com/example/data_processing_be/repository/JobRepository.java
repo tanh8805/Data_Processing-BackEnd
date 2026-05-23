c95e0d88bddcdf60c9cf2b1ed5bddbda9b76cd52
tuananhlaptrinh <bui058277@gmail.com>
2026-05-23 19:33:37 +0700

    Fix: jobrepository

--- src/main/java/com/example/data_processing_be/repository/JobRepository.java
+++ src/main/java/com/example/data_processing_be/repository/JobRepository.java
@@ -2,6 +2,8 @@ package com.example.data_processing_be.repository;
 
 import com.example.data_processing_be.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,6 @@ import java.util.UUID;
 @Repository
 public interface JobRepository extends JpaRepository<Job, UUID> {
 
-  Optional<Job> findByConversationId(UUID conversationId);
+    @Query("SELECT j FROM Job j WHERE j.conversation.id = :conversationId")
+    Optional<Job> findByConversationId(@Param("conversationId") UUID conversationId);
 }
