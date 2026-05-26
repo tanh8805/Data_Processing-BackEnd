7e6f364a976dfb1727d93fbb706ad558aa01944d
tuananhlaptrinh <bui058277@gmail.com>
2026-05-26 09:57:46 +0700

    Fix: race condition websocket

--- src/main/java/com/example/data_processing_be/DataProcessingBeApplication.java
+++ src/main/java/com/example/data_processing_be/DataProcessingBeApplication.java
@@ -1,33 +1,12 @@
 package com.example.data_processing_be;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.client.RestTemplate;
-import com.example.data_processing_be.repository.UserRepository;
-import com.example.data_processing_be.repository.ConversationRepository;
-import com.example.data_processing_be.repository.JobRepository;
-import com.example.data_processing_be.repository.JobEventRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class DataProcessingBeApplicationTests {
+@SpringBootApplication
+public class DataProcessingBeApplication {
 
-	@MockBean
-	private RestTemplate restTemplate;
-
-	@MockBean
-	private UserRepository userRepository;
-
-	@MockBean
-	private ConversationRepository conversationRepository;
-
-	@MockBean
-	private JobRepository jobRepository;
-
-	@MockBean
-	private JobEventRepository jobEventRepository;
-
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessingBeApplication.class, args);
 	}
 }
