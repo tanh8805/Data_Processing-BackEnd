350b267578f4e7a97b0471892e16cf9b43792972
tuananhlaptrinh <bui058277@gmail.com>
2026-05-26 10:00:10 +0700

    Fix: race condition websocket

--- src/test/java/com/example/data_processing_be/DataProcessingBeApplicationTests.java
+++ src/test/java/com/example/data_processing_be/DataProcessingBeApplicationTests.java
@@ -2,30 +2,9 @@ package com.example.data_processing_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.web.client.RestTemplate;
-import com.example.data_processing_be.repository.UserRepository;
-import com.example.data_processing_be.repository.ConversationRepository;
-import com.example.data_processing_be.repository.JobRepository;
-import com.example.data_processing_be.repository.JobEventRepository;
 
 @SpringBootTest
 class DataProcessingBeApplicationTests {
-
-	@MockBean
-	private RestTemplate restTemplate;
-
-	@MockBean
-	private UserRepository userRepository;
-
-	@MockBean
-	private ConversationRepository conversationRepository;
-
-	@MockBean
-	private JobRepository jobRepository;
-
-	@MockBean
-	private JobEventRepository jobEventRepository;
 
 	@Test
 	void contextLoads() {
